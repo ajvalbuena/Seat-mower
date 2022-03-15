b4276f0437897d10b098649c2e700f0c3236a855
Ana Jimenez <ana.jimenez.valbuena@gmail.com>
2022-03-15 23:23:04 +0100

    Updates app to be able to include two mowers

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -9,14 +9,16 @@ public class App {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        String[] input = new String[5];
 
-        System.out.println("Enter the initial mower: ");
-        String mover = in.nextLine();
+        System.out.println("Please, enter the upper limits of your plateau, and your mowers (with the initial position and their " +
+                "instructions):");
+        for (int i = 0; i < input.length; i++) {
+            input[i] = in.nextLine();
+        }
 
-        System.out.println("Enter the instructions: ");
-        String instructions = in.nextLine();
-
-        System.out.println(TerrainInspection.inspectWithMower("", mover, instructions));
+        System.out.println(TerrainInspection.inspectWithMower(input[0], input[1], input[2]));
+        System.out.println(TerrainInspection.inspectWithMower(input[0], input[3], input[4]));
     }
 
 
